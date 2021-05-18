c4196094c5cede3ee36e53e48aa57911d645ebaa
Julien Poissonnier <julien@caffeine.lu>
2021-05-18 09:30:14 +0200

    debug

--- src/api/upload.js
+++ src/api/upload.js
@@ -5,20 +5,20 @@ const GOOGLE_SA_CLIENT_EMAIL = process.env.GOOGLE_SA_CLIENT_EMAIL
 const GOOGLE_SA_PRIVATE_KEY = process.env.GOOGLE_SA_PRIVATE_KEY
 const BUCKET = process.env.BUCKET
 
-const storage = new Storage({
-  projectId: GOOGLE_PROJECT_ID,
-  credentials: {
-    project_id: GOOGLE_PROJECT_ID,
-    location_id: "us-central-1",
-    client_email: GOOGLE_SA_CLIENT_EMAIL,
-    private_key: GOOGLE_SA_PRIVATE_KEY,
-  },
-})
-
-const bucket = storage.bucket(BUCKET)
-
 async function handler(req, res) {
-  console.log("Hi!")
+  console.log("Hi!", { process_env: process.env, process_env: process["env"] })
+
+  const storage = new Storage({
+    projectId: GOOGLE_PROJECT_ID,
+    credentials: {
+      project_id: GOOGLE_PROJECT_ID,
+      location_id: "us-central-1",
+      client_email: GOOGLE_SA_CLIENT_EMAIL,
+      private_key: GOOGLE_SA_PRIVATE_KEY,
+    },
+  })
+
+  const bucket = storage.bucket(BUCKET)
   const [files] = await storage.bucket(bucket).getFiles()
   console.log({ files })
   res.json({ files })
