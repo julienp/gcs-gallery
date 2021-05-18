38f6b8a0d9aa4a3559661c5f895284d88137b3d8
Julien Poissonnier <julien@caffeine.lu>
2021-05-18 09:25:08 +0200

    export func

--- src/api/upload.js
+++ src/api/upload.js
@@ -1,9 +1,9 @@
-const { Storage } = require("@google-cloud/storage");
+const { Storage } = require("@google-cloud/storage")
 
-const GOOGLE_PROJECT_ID = process.env.GOOGLE_PROJECT_ID;
-const GOOGLE_SA_CLIENT_EMAIL = process.env.GOOGLE_SA_CLIENT_EMAIL;
-const GOOGLE_SA_PRIVATE_KEY = process.env.GOOGLE_SA_PRIVATE_KEY;
-const BUCKET = process.env.BUCKET;
+const GOOGLE_PROJECT_ID = process.env.GOOGLE_PROJECT_ID
+const GOOGLE_SA_CLIENT_EMAIL = process.env.GOOGLE_SA_CLIENT_EMAIL
+const GOOGLE_SA_PRIVATE_KEY = process.env.GOOGLE_SA_PRIVATE_KEY
+const BUCKET = process.env.BUCKET
 
 const storage = new Storage({
   projectId: GOOGLE_PROJECT_ID,
@@ -13,13 +13,15 @@ const storage = new Storage({
     client_email: GOOGLE_SA_CLIENT_EMAIL,
     private_key: GOOGLE_SA_PRIVATE_KEY,
   },
-});
+})
 
-const bucket = storage.bucket(BUCKET);
+const bucket = storage.bucket(BUCKET)
 
-export async function handler(req, res) {
-  console.log("Hi!");
-  const [files] = await storage.bucket(bucket).getFiles();
-  console.log({ files });
-  res.json({ files });
+async function handler(req, res) {
+  console.log("Hi!")
+  const [files] = await storage.bucket(bucket).getFiles()
+  console.log({ files })
+  res.json({ files })
 }
+
+export default handler
